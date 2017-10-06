073f9426699f9f1b739c7efda4d2f95389afd931
Itiu <ValeriyBushenev@gmail.com>
2017-10-06 21:51:37 +0300

    *QA

--- qa/testDelegationTask.js
+++ qa/testDelegationTask.js
@@ -1,7 +1,7 @@
-var basic = require('basic.js'),
-    compl/exRoute = require('complexRoute.js'),
-    delegationRequest = require('delegationRequest.js'),
-    person = require('../person.js'),
+var basic = require('./basic.js'),
+    complexRoute = require('./complexRoute.js'),
+    delegationRequest = require('./delegationRequest.js'),
+    person = require('./person.js'),
     timeStamp = ''+Math.round(+new Date()/1000),
     webdriver = require('selenium-webdriver');
     
