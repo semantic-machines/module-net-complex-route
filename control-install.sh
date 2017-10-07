d8349a2dfaaf408f6a7a9bad19f7e1fb34d9f400
Itiu <ValeriyBushenev@gmail.com>
2017-10-07 17:52:24 +0300

    *QA

--- control-install.sh
+++ control-install.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/semantic-machines/veda.git
 ln -s $PWD/ontology ./veda/ontology/module-net-complex-route
 #ln -s $PWD/qa ./veda/qa/module-net-complex-route
-ln -s $PWD/qa/*.js ./veda/qa
+#ln -s $PWD/qa/*.js ./veda/qa
 cd veda
 ./control-install.sh
