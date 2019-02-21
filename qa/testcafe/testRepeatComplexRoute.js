dd89bf91e5881562de28c6c179ee44e782bb7453
Leonid Borisov <brutal616@gmail.com>
2019-02-21 15:10:54 +0300

    fix test

--- qa/testcafe/testRepeatComplexRoute.js
+++ qa/testcafe/testRepeatComplexRoute.js
@@ -21,11 +21,11 @@ import { Selector, t } from 'testcafe';
       await t
         .click('#menu')
         .click('li[id="menu"] li[resource="v-s:Find"]')
-        .typeText('veda-control[rel="v-fs:typeToSearch"]', 'Стартовая форма Комплексного маршрута')
-        .click('div.suggestion[resource="s-wf:ComplexRouteStartForm"]')
-        .click('div.input-group button#submit')
-        .click('ul.nav.nav-tabs.nav-right li[role="presentation"] a#results-pill-ft')
-        .click('div#results-ft div#search-results p[typeof="s-wf:ComplexRouteStartForm"] strong a[data-template="v-ui:ClassNameLabelTemplate"]')
+        .typeText('veda-control[property="*"] input.form-control', 'Стартовая форма Комплексного маршрута')
+        .click('small[about="v-fs:AdvancedSearchBundle"]')
+        .click('span[about="v-s:UserThing"] button.btn.btn-default.button-delete')
+        .click('button#custom-search-button')
+        .click('table.table.table-bordered.table-condensed tbody.result-container a.glyphicon.glyphicon-search')
         .click('h4#on-document a.label-template[typeof="s-wf:ComplexRouteTest3"]')
         .click('.action#send')
         .click('div.actions button#save_and_start_process')
@@ -34,7 +34,7 @@ import { Selector, t } from 'testcafe';
     basic.checkInbox('1');
     basic.sendInbox();
       await t
-        .expect(Selector('ul.nav.navbar-nav.navbar-right li#user-info').innerText).eql('7 Администратор7\n');
+        .expect(Selector('ul.nav.navbar-nav.navbar-right li#user-info').innerText).eql('Администратор7 7 ./n');
     basic.logout();
 
 });
