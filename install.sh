0557765d30b817367804a1a9c76d7494252a0d06
itiu <ValeriyBushenev@gmail.com>
2019-02-22 17:10:00 +0300

    *veda 5.2.1

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VEDA_VERSION=5.2.0
+VEDA_VERSION=5.2.1
 PROJECT_NAME=module-net-complex-route
 
 wget https://github.com/semantic-machines/veda/archive/v$VEDA_VERSION.zip
@@ -10,4 +10,5 @@ mkdir $PWD/ontology/$PROJECT_NAME
 cp -r $PWD/qa/data/* $PWD/ontology/$PROJECT_NAME
 cp -r $PWD/onto/* $PWD/ontology/$PROJECT_NAME
 cp $PWD/module.ttl $PWD/ontology/$PROJECT_NAME/module-install.ttl
+cp $PWD/qa/data/* $PWD/ontology/$PROJECT_NAME/test-data
 ./control-install.sh
